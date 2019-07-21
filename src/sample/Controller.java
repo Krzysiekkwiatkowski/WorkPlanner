c283eed87e338931a910b5b1be226c32b625c302
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2019-07-21 10:54:11 +0200

    Added context menu to delete drivers

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,8 +8,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.util.Callback;
 import sample.data.Driver;
 import sample.data.DriverData;
+
 import java.io.IOException;
 import java.util.Optional;
 
@@ -21,45 +23,76 @@ public class Controller {
     @FXML
     private ListView<Driver> driversList;
 
-    public void initialize(){
+    @FXML
+    private ContextMenu listContextMenu;
+
+    public void initialize() {
+        listContextMenu = new ContextMenu();
+        MenuItem deleteMenuItem = new MenuItem("Usuń");
+        deleteMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Driver driver = driversList.getSelectionModel().getSelectedItem();
+                confirmDeletion(driver);
+            }
+        });
+
+        listContextMenu.getItems().addAll(deleteMenuItem);
         driversList.getItems().setAll(DriverData.getDrivers());
+        driversList.setCellFactory(new Callback<ListView<Driver>, ListCell<Driver>>() {
+            @Override
+            public ListCell<Driver> call(ListView<Driver> param) {
+                ListCell<Driver> cell = new ListCell<Driver>() {
+                    @Override
+                    protected void updateItem(Driver item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setText(null);
+                        } else {
+                            setText(item.toString());
+                        }
+                    }
+                };
+
+                cell.emptyProperty().addListener(
+                        (obs, wasEmpty, isNowEmpty) -> {
+                            if (isNowEmpty) {
+                                cell.setContextMenu(null);
+                            } else {
+                                cell.setContextMenu(listContextMenu);
+                            }
+                        }
+                );
+                return cell;
+            }
+        });
     }
 
     @FXML
-    public void exitAction(){
+    public void exitAction() {
         DriverData.saveDrivers();
         Platform.exit();
     }
 
     @FXML
-    public void showNewDriverDialog(){
+    public void showNewDriverDialog() {
         Dialog<ButtonType> dialog = new Dialog<>();
         dialog.initOwner(mainBorderPane.getScene().getWindow());
         dialog.setTitle("Dodaj nowego kierowcę");
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("driverDialog.fxml"));
-        try{
+        try {
             dialog.getDialogPane().setContent(fxmlLoader.load());
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Couldn't load the dialog");
             e.printStackTrace();
         }
         dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
         dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
         Optional<ButtonType> result = dialog.showAndWait();
-        if(result.isPresent() && result.get() == ButtonType.OK){
+        if (result.isPresent() && result.get() == ButtonType.OK) {
             DriverController controller = fxmlLoader.getController();
             driversList.getItems().add(controller.processResult());
-        }
-    }
-
-    @FXML
-    public void onRightClick(MouseEvent event){
-        Driver driver = driversList.getSelectionModel().getSelectedItem();
-        if(driver != null){
-            if(event.getEventType().equals(MouseEvent.MOUSE_CLICKED)){
-                confirmDeletion(driver);
-            }
         }
     }
 
@@ -67,9 +100,10 @@ public class Controller {
     private void confirmDeletion(Driver driver) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Usunąć kierowcę?");
+        alert.setHeaderText("Usunąć " + driver.toString());
         alert.setContentText("Kliknij OK żeby potwierdzić, Cancel żeby anulować");
         Optional<ButtonType> result = alert.showAndWait();
-        if(result.isPresent() && result.get() == ButtonType.OK){
+        if (result.isPresent() && (result.get() == ButtonType.OK)) {
             DriverData.deleteDriver(driver);
             driversList.getItems().removeAll(driver);
         }
